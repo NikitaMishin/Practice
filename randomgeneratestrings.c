28133f37d0edeb209bdf1a6d8b711e9dd5a4b6d4
NikitaMishin <nikitamishin97@mail.com>
2016-10-19 23:18:33 +0300

    fixed randomgenerate and added testingproggram

--- randomgeneratestrings.c
+++ randomgeneratestrings.c
@@ -5,32 +5,34 @@
 
 int main(void)
 { 
+	srand ( time(NULL) );
 	int numb = 100;
 	FILE * file[numb];
+	int p;
 for (int k=0;k<numb;k++)
 {
-
- srand ( time(NULL) ); //ok
- long int size = rand()&100000+ 2000;
+ long int size = rand()%30000+ 100;
  printf("=%ld=",size);
  char**strings= (char**)malloc(sizeof(char*)*size);
  int j;
+ int z;
  for(int i = 0; i<size;i++)
  { 
-  long int c = rand()%120 + 20;
+   int c = rand()%120 + 20;
  	strings[i] = (char*)malloc(sizeof(char)*(c+2));
- 	for( j = 0 ;j < c;j++)
+ 	for( j = 0 ;j < c;j++)	
  	{
- 		strings[i][j]= rand()%58 + 65;
- 		 while(!(( (strings[i][j] = rand()%58+65)<=122 )&&(strings[i][j]>=65)&&(strings[i][j]!=91)&&(strings[i][j]!=92)&&(strings[i][j]!=93)&&(strings[i][j]!=94)&&(strings[i][j]!=95&&(strings[i][j]!=96)))) ;  //91 96
+ 		 while(( (( (z = rand()%58+65))<=122 ) && (z>=65) && (z!=91) && (z!=92) && (z!=93) && (z!=94) && (z!=95) && (z!=96))==0 ) ;   
+ 		 strings[i][j]=z;
  	} 
- 	strings[i][j] ='\n';
+ 	
+ 	strings[i][j] ='\0';
  }
  char filename[20];
  sprintf(filename,"input%d.txt",k+1);
 file[k] = fopen(filename,"w");
 fprintf(file[k],"%ld\n",size);
-for(int i = 0; i<size;i++) fprintf(file[k],"%s",strings[i]);
+for(int i = 0; i<size;i++) fprintf(file[k],"%s\n",strings[i]);
 fclose(file[k]);
 }
 
