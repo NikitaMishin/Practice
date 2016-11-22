08fbffef2d9d3679d4b3abc293d2fa1bf2427a42
NikitaMishin <nikitamishin97@mail.com>
2016-11-22 23:25:09 +0300

    just unserious add

--- randomgeneratestrings.c
+++ randomgeneratestrings.c
@@ -2,27 +2,29 @@
 #include <time.h>
 #include <stdlib.h>
 
-
+// just generate file with many strings
 int main(void)
 { 
 	srand ( time(NULL) );
-	int numb = 100;
+	int numb = 101;
 	FILE * file[numb];
 	int p;
+	long int * n = (long int*) malloc(sizeof(long int)*numb);
 for (int k=0;k<numb;k++)
 {
 
- long int size = rand()%30000+ 100;
-
+ long int size = rand()%500000+ 100000;
+ 
  char**strings= (char**)malloc(sizeof(char*)*size);
  int j;
  int z;
  for(int i = 0; i<size;i++)
  { 
-   int c = rand()%120 + 20;
+   int c = rand()%420 + 120;
  	strings[i] = (char*)malloc(sizeof(char)*(c+2));
  	for( j = 0 ;j < c;j++)	
  	{
+ 	
  		 while(( (( (z = rand()%58+65))<=122 ) && (z>=65) && (z!=91) && (z!=92) && (z!=93) && (z!=94) && (z!=95) && (z!=96))==0 ) ;   
  		 strings[i][j]=z;
  	} 
@@ -32,10 +34,12 @@ for (int k=0;k<numb;k++)
  char filename[20];
  sprintf(filename,"input%d.txt",k+1);
 file[k] = fopen(filename,"w");
-fprintf(file[k],"%ld\n",size);
 for(int i = 0; i<size;i++) fprintf(file[k],"%s\n",strings[i]);
 fclose(file[k]);
+n[k]= size;
 }
-
+file[100]=fopen("quantity.txt","w");
+for (int i = 0 ;i<numb;i++){fprintf(file[100],"%ld\n",n[i]);}
+fclose(file[100]);
 return 0;
 }
